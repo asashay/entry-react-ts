e532ce2bb70031f8718dd00a28745f2709f89ed3
Alex Oliynyk <asashayko@gmail.com>
2022-04-15 16:29:57 +0300

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useAuth } from "react-oidc-context";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const auth = useAuth();
+
+    const handleLogoutClick = () => {
+      auth.signoutRedirect();
+    }
+
+    const btnStyle = { 
+      padding: '10px 20px', 
+      backgroundColor: 'black', 
+      color: 'white', 
+      borderRadius: '5px'
+    };
+
+    switch (auth.activeNavigator) {
+        case "signinSilent":
+          return <div>Signing you in...</div>;
+        case "signoutRedirect":
+            return <div>Signing you out...</div>;
+    }
+
+    if (auth.isLoading) {
+      return <div>Loading...</div>;
+    }
+
+    if (auth.error) {
+      return <div>
+        <p>Oops... {auth.error.message}</p>
+        <button 
+          style={btnStyle} 
+          onClick={handleLogoutClick}>Go Back</button>
+      </div>;
+    }
+
+    if (auth.isAuthenticated) {
+      return (
+        <div style={{ display: 'flex', flexDirection: 'column', maxWidth: '320px'}}>
+          <h3>Your user data:</h3>
+          <pre style={{ padding: '20px'}}>
+            {JSON.stringify(auth.user?.profile, null, 2)}
+          </pre>
+          <button style={btnStyle} onClick={handleLogoutClick}>Log out</button>
+        </div>
+        );
+    } 
+
+    return <div>
+      <button 
+        onClick={() => void auth.signinRedirect({extraQueryParams: { prompt: 'login'}})}
+        style={btnStyle}
+      >Log in</button>
     </div>
-  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from "react-oidc-context";
+import App from './app';
+
+import { oidcConfig  } from './oidc-config';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider {...oidcConfig} response_mode="fragment">
+      <div style={{ 
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        height: '100vh'
+        }}>
+        <App />
+      </div>
+    </AuthProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
